ecca43109fa010abf4141e31a2321463a7fdee8e
Diego19890102 <diegomonfort1989@gmail.com>
2023-05-17 19:10:13 +0200

    update 3
    finish

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ const navSlide = () => {
         nav.classList.toggle('nav-active');
            //Animate Links
         navLinks.forEach((link, index) => {
-        if(link.style.animation){
-            link.style.animation = ''
+        if (link.style.animation) {
+            link.style.animation = '';
         } else {
-            link.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + 1.5}s`;
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
            
          }
       });
+
+      //burger animation
+      burger.classList.toggle('toggle')
+
     });
 }
 
